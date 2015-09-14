5bf7614948a637f5c6cc6ecdcb622831d3f31a37
hzsunshx <hzsunshx@corp.netease.com>
2015-09-14 11:07:48 +0800

    add -proto

--- main.go
+++ main.go
@@ -18,68 +18,79 @@ const (
 	VERSION = "0.1"
 )
 
-func main() {
-	var serverMode bool
-	var serverAddr string
-	var proxyPort int
-	var proxyAddr string
-	var subDomain string
-	var domain string
-	var debug bool
+type GlobalConfig struct {
+	Server struct {
+		Enable bool
+		Addr   string
+		Domain string
+	}
 
+	Proto     string
+	ProxyPort int
+	SubDomain string
+	Debug     bool
+}
+
+var cfg GlobalConfig
+
+func init() {
 	var defaultServerAddr = os.Getenv("PXL_SERVER_ADDR")
 	if defaultServerAddr == "" {
 		defaultServerAddr = "proxylocal.xyz"
 	}
-	flag.BoolVar(&serverMode, "server", false, "run in server mode")
-	flag.StringVar(&serverAddr, "server-addr", defaultServerAddr, "server address")
-	flag.StringVar(&domain, "server-domain", "", "proxy server domain name, optional")
-	flag.StringVar(&subDomain, "subdomain", "", "proxy subdomain, used for http")
-	flag.BoolVar(&debug, "debug", false, "open debug mode")
-	flag.IntVar(&proxyPort, "port", 0, "proxy server listen port, used for tcp")
+	flag.BoolVar(&cfg.Debug, "debug", false, "open debug mode")
+	flag.BoolVar(&cfg.Server.Enable, "server", false, "run in server mode")
+	flag.StringVar(&cfg.Server.Addr, "server-addr", defaultServerAddr, "server address")
+	flag.StringVar(&cfg.Server.Domain, "server-domain", "", "proxy server domain name, optional")
+	flag.StringVar(&cfg.SubDomain, "subdomain", "", "proxy subdomain, used for http")
+	flag.StringVar(&cfg.Proto, "proto", "http", "default protocal, http or tcp")
+	flag.IntVar(&cfg.ProxyPort, "port", 0, "proxy server listen port, used for tcp")
 
 	flag.Usage = func() {
 		fmt.Printf("proxylocal version: %v\nUsage: %s [OPTIONS] <port | host:port>\n", VERSION, os.Args[0])
 		flag.PrintDefaults()
 	}
+}
+
+func main() {
 	flag.Parse()
-	if !serverMode && len(flag.Args()) != 1 {
+	if !cfg.Server.Enable && len(flag.Args()) != 1 {
 		flag.Usage()
 		return
 	}
-	if !debug {
+	if !cfg.Debug {
 		log.SetOutputLevel(log.Linfo)
 	} else {
 		log.SetOutputLevel(log.Ldebug)
 	}
 
-	if serverMode {
-		_, port, _ := net.SplitHostPort(serverAddr)
+	if cfg.Server.Enable {
+		_, port, _ := net.SplitHostPort(cfg.Server.Addr)
 		if port == "" {
 			port = "80"
 		}
 		addr := net.JoinHostPort("0.0.0.0", port)
-		if domain == "" {
-			domain = serverAddr
+		if cfg.Server.Domain == "" {
+			cfg.Server.Domain = cfg.Server.Addr
 		}
 		fmt.Println("proxylocal: server listen on", addr)
-		ps := pxlocal.NewProxyServer(domain)
+		ps := pxlocal.NewProxyServer(cfg.Server.Domain)
 		log.Fatal(http.ListenAndServe(addr, ps))
 	}
 
-	proxyAddr = flag.Arg(0)
+	var proxyAddr = flag.Arg(0)
 	if !regexp.MustCompile("^(http|https|tcp)://").MatchString(proxyAddr) {
 		if _, err := strconv.Atoi(proxyAddr); err == nil { // only contain port
 			proxyAddr = "localhost:" + proxyAddr
 		} else {
 			//proxyAddr += ":80"
 		}
-		proxyAddr = "http://" + proxyAddr
+		proxyAddr = cfg.Proto + "://" + proxyAddr
 	}
 	pURL, err := url.Parse(proxyAddr)
 	if err != nil {
 		log.Fatal(err)
 	}
 	log.Println("proxy URL:", pURL)
-	pxlocal.StartAgent(pURL, subDomain, serverAddr, proxyPort)
+	pxlocal.StartAgent(pURL, cfg.SubDomain, cfg.Server.Addr, cfg.ProxyPort)
 }
